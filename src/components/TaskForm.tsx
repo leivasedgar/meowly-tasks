36a43bfe4944beb1c2bbe00920b3c155532b64a6
Edgar Leiva <leiva_2307@hotmail.com>
2025-07-17 21:20:14 -0600

    Updated TaskForm.tsx

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -242,7 +242,7 @@ export const TaskForm: React.FC<TaskFormProps> = ({ isOpen, onClose, onSave, edi
                     <i 
                       className={`fas ${
                         priority === 'high' ? 'fa-radiation' :
-                        priority === 'medium' ? 'fa-triangle-exclamation' : 'fa-shield-cat'
+                        priority === 'medium' ? 'fa-triangle-exclamation' : 'fa-exclamation'
                       } mb-1`}
                         style={{
                           color: isSelected ? Colors.utility.secondaryBackground : priorityColors[priority]
